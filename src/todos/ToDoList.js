5fafd03272c70af9d619ce53293c898fed20fdbe
SilviuDN <silviudilimot@gmail.com>
2021-08-24 22:42:24 +0300

    getting data from server

--- src/todos/ToDoList.js
+++ src/todos/ToDoList.js
@@ -1,28 +1,50 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import {connect} from 'react-redux'
 import {removeTodo, completedTodo} from './actions'
+import {displayAlert} from './thunks'
+import { loadTodos } from './thunks';
 import ToDoListItem from './ToDoListItem'
 import NewToDoForm from './NewToDoForm'
 import "./ToDoList.css"
 
-const ToDoList = ({todos =[], onRemovePressed, onCompletePressed}) => (
-    <div className="list-wrapper">
-        <NewToDoForm/>
-        {todos.map( todo => <ToDoListItem 
-            todo = {todo} 
-            onRemovePressed = {onRemovePressed}
-            onCompletePressed = {onCompletePressed}
-        />)}
-    </div>
-)
+const ToDoList = ({ todos = [], onRemovePressed, onCompletePressed, isLoading, startLoadingTodos }) => {
+    useEffect(() => {
+        startLoadingTodos();
+    }, []);
+    
+    const loadingMessage = <div>Loading todos...</div>;
+    const content = (
+        <div className="list-wrapper">
+            <NewToDoForm />
+            {todos.map(todo => <ToDoListItem
+                todo={todo}
+                onRemovePressed={onRemovePressed}
+                onCompletePressed={onCompletePressed}/>)}
+        </div>
+    );
+    return isLoading ? loadingMessage : content;
+};
+
+// const ToDoList = ({todos =[], onRemovePressed, onCompletePressed}) => (
+//     <div className="list-wrapper">
+//         <NewToDoForm/>
+//         {todos.map( todo => <ToDoListItem 
+//             todo = {todo} 
+//             onRemovePressed = {onRemovePressed}
+//             onCompletePressed = {onCompletePressed}
+//         />)}
+//     </div>
+// )
 
 const mapStateToProps = state => ({
+    isLoading: state.isLoading,
     todos: state.todos,
 });
 
 const mapDispatchToProps = dispatch => ({
+    startLoadingTodos: () => dispatch(loadTodos()),
     onRemovePressed: text => dispatch(removeTodo(text)),
-    onCompletePressed: text => dispatch(completedTodo(text))
+    onCompletePressed: text => dispatch(completedTodo(text)),
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ToDoList) 
--- src/todos/actions.js
+++ src/todos/actions.js
@@ -1,5 +1,6 @@
 // ACTION CREATOR - extracted the actual code for the action
 export const CREATE_TODO = 'CREATE_TODO';
+// Action creator:
 export const createTodo = text => ({
     type: CREATE_TODO,
     payload: { text },
@@ -15,4 +16,20 @@ export const COMPLETED_TODO = 'COMPLETED_TODO'
 export const completedTodo = text => ({
     type: COMPLETED_TODO,
     payload: { text },
+});
+
+export const LOAD_TODOS_IN_PROGRESS = 'LOAD_TODOS_IN_PROGRESS';
+export const loadTodosInProgress = () => ({
+    type: LOAD_TODOS_IN_PROGRESS,
+});
+
+export const LOAD_TODOS_SUCCESS = 'LOAD_TODOS_SUCCESS';
+export const loadTodosSuccess = todos => ({
+    type: LOAD_TODOS_SUCCESS,
+    payload: { todos },
+});
+
+export const LOAD_TODOS_FAILURE = 'LOAD_TODOS_FAILURE';
+export const loadTodosFailure = () => ({
+    type: LOAD_TODOS_FAILURE,
 });
